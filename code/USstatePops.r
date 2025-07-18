33631a793378a90bc71de11ed00eaf7075544973
flahertyc <122467714+coco3427@users.noreply.github.com>
2025-07-18 14:58:42 -0400

    Update USstatePops.r

--- code/USstatePops.r
+++ code/USstatePops.r
@@ -1,5 +1,3 @@
 library(readxl)
 download.file("https://raw.githubusercontent.com/pearcej/intro2datasci/main/code/nst-est2019-0-clean.xlsx", tmp <- tempfile(fileext = ".xlsx"))
 USstatePops <- read_excel(tmp, col_names = FALSE)
-2:49
-print(paste("any string", variable))
